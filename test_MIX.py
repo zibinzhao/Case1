248dd56357c341cb969faf646d0193b77f5c74ee
zibinzhao <48482836+zibinzhao@users.noreply.github.com>
2024-11-07 15:17:03 +0000

    Update test_MIX.py

--- test_MIX.py
+++ test_MIX.py
@@ -220,7 +220,7 @@ if classifier == "Random Forest classification":
         st.write('Which features caused this specific prediction? features in **red increased** the prediction, in **blue decreased** them.')
         st.write('The **base value** in the force plot shows the **average predicted customer satisfied probability** of this classification model.')
         force_plot_1 = shap.force_plot(explainer.expected_value[1], shap_values_1[1], x_test.iloc[individual,:])
-        st_shap(force_plot_1)
+        st.pyplot(force_plot_1)
 
 
         st.subheader("2) Interactive Force Plot")
@@ -230,7 +230,7 @@ if classifier == "Random Forest classification":
                      ' This interactive force plot can explain the **predictions of multiple instances** in one plot.'
                     ' The Y-axis is the X-axis of the individual force plot. There are 865 data points in the X_test, so the X-axis has 865 observations.')
         force_plot_2 = shap.force_plot(explainer.expected_value[1], shap_values[1], x_test)
-        st_shap(force_plot_2, height=400)
+        st.pyplot(force_plot_2, height=400)
         
 
         #############Global interpretiability###############
@@ -342,7 +342,7 @@ if classifier == "Random Forest Regression":
         st.write('Which features caused this specific prediction? features in **red increased** the prediction, in **blue decreased** them.')
         st.write('The **base value** in the force plot shows the **average predicted customer satisfication score** of this regression model.')
         force_plot_3 = shap.force_plot(explainer_1.expected_value, shap_values_2[individual_1,:], X_test.iloc[individual_1,:])
-        st_shap(force_plot_3)
+        st.pyplot(force_plot_3)
         
 
   
@@ -353,7 +353,7 @@ if classifier == "Random Forest Regression":
                      ' This interactive force plot can explain the predictions of multiple instances in one plot.'
                       ' The Y-axis is the X-axis of the individual force plot. There are 865 data points in the X_test, so the X-axis has 865 observations.')
         force_plot_4 = shap.force_plot(explainer_1.expected_value, shap_values_2, X_test)
-        st_shap(force_plot_4, height=400)
+        st.pyplot(force_plot_4, height=400)
         
         
 
